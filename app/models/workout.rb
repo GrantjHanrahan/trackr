1f1f279ee84d6023c9ba4f113708748113d32597
Grant Hanrahan <grantjhanrahan@gmail.com>
2018-01-22 19:13:29 +1100

    added styling to db, login pages

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,6 @@
 class Workout < ApplicationRecord
    belongs_to :user, optional: true
+   belongs_to :plan, optional: true
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,12 @@ user1 = User.create name:'Grant Test', email:'grant@test.co', password: 'chicken
 
 user2 = User.create name:'Grantelia Test2', email: 'grantelia@test.co', password: 'chicken', avatar: 'https://jsns.eu/components/com_jsn/assets/img/default.jpg', age:22, height:1.65, weight:62
 puts "Created #{ User.all.length } users."
+
+Workout.destroy_all
+workout1 = Workout.create workout_name: "Bay Run", date: 2018/02/22
+puts "Created #{ Workout.all.length } workouts."
+
+workout2 = Workout.create workout_name: "Bay Run 2", date: '2018/02/24'
+puts "Created #{ Workout.all.length } workouts."
+
+user1.workouts << workout1 << workout2
